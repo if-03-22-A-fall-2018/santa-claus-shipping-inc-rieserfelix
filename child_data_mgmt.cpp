c0ca6ffbe468ad0c8bdd0553c9ffa22908c4367f
rieserfelix <dafelix.rieser@gmail.com>
2018-12-18 11:45:23 +0100

    undone

---
+++ child_data_mgmt.cpp
@@ -0,0 +1,18 @@
+#include "child_data_mgmt.h"
+
+ChildDataMgmt* cdm_create()
+{
+  return 0;
+}
+void cdm_add_data(ChildDataMgmt *cdm, ChildData *data)
+{
+
+}
+Node* cdm_get_sorted_data(ChildDataMgmt *cdm)
+{
+  return 0;
+}
+void cdm_delete(ChildDataMgmt *cdm)
+{
+
+}
--- list.cpp
+++ list.cpp
@@ -1,4 +1,71 @@
 #include <stdlib.h>
 #include "list.h"
 
+struct _node{
 
+  void* _data;
+  struct _node* _next;
+
+};
+
+struct _list{
+
+  struct _node* _head;
+  struct _node* node;
+
+};
+
+
+
+List* list_create()
+{
+  List* list = (List*)malloc(sizeof(struct _list));
+  return list;
+}
+
+void list_delete(List *list)
+{
+  free(list);
+}
+
+void list_add(List *list, void *data)
+{
+  Node *newNode = (Node*)malloc(sizeof(Node));
+  newNode->_data = data;
+
+  if (list->_head == 0) {
+    list->_head = newNode;
+  }
+
+  else{
+    newNode->_next = list->_head;
+    list->_head = newNode;
+  }
+}
+
+
+
+
+
+
+
+void list_insert_after(Node *node, void *data)
+{
+  Node *newNode = (Node*)malloc(sizeof(Node));
+
+  node->_next = newNode;
+
+  newNode->_data = data;
+}
+Node* list_get_first(List *list)
+{
+  return list->_head;
+}
+Node* list_get_next(Node *node)
+{
+  return node->_next;
+}
+void* list_get_data(Node *node)
+{
+ return node->_data;
+}
